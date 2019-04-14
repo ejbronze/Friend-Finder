df421b7d9a878eddb295a23ca9918cb06ece0c09
Eddie Jaquez <erjaquez@gmail.com>
2019-04-14 10:46:53 -0400

    Added home.html structure

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// For serving of static CSS
+app.use(express.static(__dirname + "/app/css"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
